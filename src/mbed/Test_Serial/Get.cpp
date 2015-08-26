2dd1cca857ae7dea0573f7f5ca7e4a524fb64a0c
DreamSoul <diao_em@hotmail.com>
2015-08-26 02:30:46 -0400

    Get added

---
+++ src/mbed/Test_Serial/Get.cpp
@@ -0,0 +1,67 @@
+#include "Get.h"
+
+
+string getIMU_Y() {
+    return;
+}
+
+string getIMU_P() {
+    return;
+}
+
+string getIMU_R() {
+    return;
+}
+
+string getGPS_Quality() {
+    return;
+}
+
+string getGPS_UTC() {
+    return;
+}
+
+string getGPS_quality() {
+    return;
+}
+
+string getGPS_Latitude() {
+    return;
+}
+
+string getGPS_Longtitude() {
+    return;
+}
+
+string getGPS_Altitude() {
+    return;
+}
+
+string getGPS_Num_Satellite() {
+    return;
+}
+
+string getGPS_HDOP() {
+    return;
+}
+
+string getGPS_VDOP() {
+    return;
+}
+
+string getGPS_PDOP() {
+    return;
+}
+
+string getGPS_Date() {
+    return;
+}
+
+string getGPS_VelocityKnot() {
+    return;
+}
+
+string getGPS_VelocityKph() {
+    return;
+}
+
---
+++ src/mbed/Test_Serial/Get.h
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+
+string getIMU_Y();
+string getIMU_P();
+string getIMU_R();
+string getGPS_Quality();
+string getGPS_UTC();
+string getGPS_quality();
+string getGPS_Latitude();
+string getGPS_Longtitude();
+string getGPS_Altitude();
+string getGPS_Num_Satellite();
+string getGPS_HDOP();
+string getGPS_VDOP();
+string getGPS_PDOP();
+string getGPS_Date();
+string getGPS_VelocityKnot();
+string getGPS_VelocityKph();
